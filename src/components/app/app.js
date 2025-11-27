17714d89a071f381c27fb365bf78b91367bed3d4
Arina Zhukova <Azhukova19842005@yandex.ru>
2025-11-27 17:52:53 +0300

    first commit

---
+++ src/components/app/app.js
@@ -0,0 +1,25 @@
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmploersList from '../emploers-list/emploers-list';
+import EmploersAddForm from '../emploers-add-form/emploers-add-form';
+import './app.css'
+
+function App() {
+    return (
+        <div className="app">
+            <AppInfo />
+
+            <div className="search-panel">
+                <SearchPanel />
+                <AppFilter />
+            </div>
+
+            <EmploersList />
+            <EmploersAddForm />
+
+        </div>
+    );
+}
+
+export default App
